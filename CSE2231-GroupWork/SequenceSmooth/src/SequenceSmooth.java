8413084a85a5ee0f5d32f44483a82c11e965a16a
TonyLiou4 <tonyliou4@gmail.com>
2022-01-27 22:55:08 -0500

    updaste

--- CSE2231-GroupWork/SequenceSmooth/src/SequenceSmooth.java
+++ CSE2231-GroupWork/SequenceSmooth/src/SequenceSmooth.java
@@ -1,7 +1,5 @@
 import components.sequence.Sequence;
 import components.sequence.Sequence1L;
-import components.simplewriter.SimpleWriter;
-import components.simplewriter.SimpleWriter1L;
 
 /**
  * Implements method to smooth a {@code Sequence<Integer>}.
@@ -35,55 +33,12 @@ public final class SequenceSmooth {
      *        s2 = c * <(i+j)/2> * d))
      * </pre>
      */
-    public static void smooth1(Sequence<Integer> s1, Sequence<Integer> s2) {
+    public static void smooth(Sequence<Integer> s1, Sequence<Integer> s2) {
         assert s1 != null : "Violation of: s1 is not null";
         assert s2 != null : "Violation of: s2 is not null";
         assert s1 != s2 : "Violation of: s1 is not s2";
         assert s1.length() >= 1 : "Violation of: |s1| >= 1";
 
-        //******All these methods are instance methods
-        
-        Sequence<Integer> s2 = new Sequence1L<>();
-      int pos = 0;
-
-      if (s1.length() == 1) {
-          return s2;
-      } else {
-          double first = s1.remove(0);
-          double second = s1.entry(1);
-          double avg = (first + second) / 2.0;
-
-          s2.add(pos, (int) avg);
-          pos++;
-
-          smooth(s1);
-      }
-
-    /**
-     * Smooths a given {@code Sequence<Integer>}.
-     *
-     * @param s1
-     *            the sequence to smooth
-     * @param s2
-     *            the resulting sequence
-     * @replaces s2
-     * @requires |s1| >= 1
-     * @ensures <pre>
-     * |s2| = |s1| - 1  and
-     *  for all i, j: integer, a, b: string of integer
-     *      where (s1 = a * <i> * <j> * b)
-     *    (there exists c, d: string of integer
-     *       (|c| = |a|  and
-     *        s2 = c * <(i+j)/2> * d))
-     * </pre>
-     */
-    public static void smooth2(Sequence<Integer> s1, Sequence<Integer> s2) {
-        assert s1 != null : "Violation of: s1 is not null";
-        assert s2 != null : "Violation of: s2 is not null";
-        assert s1 != s2 : "Violation of: s1 is not s2";
-        assert s1.length() >= 1 : "Violation of: |s1| >= 1";
-
-        //****** All these methods are instance methods
         Sequence<Integer> seq = new Sequence1L<>();
 
         if (s1.length() == 1) {
@@ -100,5 +55,41 @@ public final class SequenceSmooth {
         s2.transferFrom(seq);
     }
 
+    /**
+     * Smooths a given {@code Sequence<Integer>}.
+     *
+     * @param s1
+     *            the sequence to smooth
+     * @returns s2 the smoothed sequence that is returned
+     * @requires |s1| >= 1
+     * @ensures <pre>
+     * |s2| = |s1| - 1  and
+     *  for all i, j: integer, a, b: string of integer
+     *      where (s1 = a * <i> * <j> * b)
+     *    (there exists c, d: string of integer
+     *       (|c| = |a|  and
+     *        s2 = c * <(i+j)/2> * d))
+     * </pre>
+     */
+   public static Sequence<Integer> smooth(Sequence<Integer> s1) {
+        assert s1 != null : "Violation of: s1 is not null";
+        assert s1.length() >= 1 : "Violation of: |s1| >= 1";
+
+       Sequence<Integer> s2 = new Sequence1L<>();
+        int pos = 0;
+
+        if (s1.length() == 1) {
+            return s2;
+        } else {
+            double first = s1.remove(0);
+            double second = s1.entry(1);
+            double avg = (first + second) / 2.0;
+
+            s2.add(pos, (int) avg);
+            pos++;
+
+            smooth(s1);
+        }
+    }
 
 }
