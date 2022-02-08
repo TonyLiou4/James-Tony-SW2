e296419d1620883ca2f6cbed35aea8391bebf851
TonyLiou4 <tonyliou4@gmail.com>
2022-02-08 15:30:29 -0500

    gyhuklkjhbghuio

--- CSE2231-GroupWork/MapWithHashing/test/MapTest.java
+++ CSE2231-GroupWork/MapWithHashing/test/MapTest.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import components.map.Map;
 
 /**
@@ -80,4 +84,230 @@ public abstract class MapTest {
     // TODO - add test cases for constructor, add, remove, removeAny, value,
     // hasKey, and size
 
+    /*
+     * Test case for the constructor
+     */
+    @Test
+    public void testNoArgsConstructor_Empty() {
+
+        //  Set up variables and call method under test
+
+        Map<String, String> test = this.constructorTest();
+        Map<String, String> expected = this.constructorRef();
+
+        //  Assert that values of variables match expectations
+
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testConstructor() {
+
+        //  Set up variables and call method under test
+
+        Map<String, String> test = this.createFromArgsTest("0", "0");
+        Map<String, String> expected = this.createFromArgsRef("0", "0");
+
+        //  Assert that values of variables match expectations
+        assertEquals(expected, test);
+    }
+
+    /**
+     * Test add method
+     */
+
+    @Test
+    public void testAdd_ZeroSize() {
+
+        Map<String, String> test = this.createFromArgsTest();
+        Map<String, String> expected = this.createFromArgsRef("James", "1");
+        test.add("James", "1");
+
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testAdd_Normal() {
+
+        Map<String, String> test = this.createFromArgsTest("James", "1");
+        Map<String, String> expected = this.createFromArgsRef("James", "1",
+                "Tony", "2");
+        test.add("Tony", "2");
+
+        assertEquals(expected, test);
+    }
+
+    /**
+     * Test remove method
+     */
+
+    @Test
+    public void testRemove1() {
+
+        Map<String, String> test = this.createFromArgsTest("James", "1");
+        Map<String, String> expected = this.createFromArgsRef("James", "1");
+        Map.Pair<String, String> pair = test.remove("James");
+        Map.Pair<String, String> pairExpected = expected.remove("James");
+
+        assertEquals(pairExpected, pair);
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testRemove2() {
+
+        Map<String, String> test = this.createFromArgsTest("James", "1", "Tony",
+                "2");
+        Map<String, String> expected = this.createFromArgsRef("Tony", "2");
+        test.remove("James");
+
+        assertEquals(expected, test);
+    }
+
+    /**
+     * Test removeAny method
+     */
+
+    @Test
+    public void testRemoveAny_OneElement() {
+
+        Map<String, String> test = this.createFromArgsTest("James", "1");
+        Map<String, String> expected = this.createFromArgsTest("James", "1");
+        Map.Pair<String, String> pair = test.removeAny();
+
+        assertEquals(true, expected.hasKey(pair.key()));
+        Map.Pair<String, String> pairExpected = expected.remove(pair.key());
+        assertEquals(pairExpected, pair);
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testRemoveAny_TwoElement() {
+
+        Map<String, String> test = this.createFromArgsTest("James", "1", "Tony",
+                "2");
+        Map<String, String> expected = this.createFromArgsTest("James", "1",
+                "Tony", "2");
+        Map.Pair<String, String> pair = test.removeAny();
+
+        assertEquals(true, expected.hasKey(pair.key()));
+        Map.Pair<String, String> pairExpected = expected.remove(pair.key());
+        assertEquals(pairExpected, pair);
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testRemoveAny_TElement() {
+
+        Map<String, String> test = this.createFromArgsTest("James", "1", "Tony",
+                "2", "Bob", "3");
+        Map<String, String> expected = this.createFromArgsTest("James", "1",
+                "Tony", "2", "Bob", "3");
+        Map.Pair<String, String> pair = test.removeAny();
+
+        assertEquals(true, expected.hasKey(pair.key()));
+        Map.Pair<String, String> pairExpected = expected.remove(pair.key());
+        assertEquals(pairExpected, pair);
+        assertEquals(expected, test);
+    }
+
+    /**
+     * Test value method
+     */
+
+    @Test
+    public void testValue1() {
+
+        Map<String, String> map = this.createFromArgsTest("James", "1", "Tony",
+                "2");
+        String test = map.value("Tony");
+        String expected = "2";
+
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testValue2() {
+
+        Map<String, String> map = this.createFromArgsTest("James", "1", "Tony",
+                "2");
+        String test = map.value("James");
+        String expected = "1";
+
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testValue() {
+        Map<String, String> map = this.createFromArgsTest("James", "1", "Tony",
+                "2", "John", "10000000000000000000");
+        String test = map.value("John");
+        String expected = "10000000000000000000";
+
+        assertEquals(expected, test);
+    }
+
+    /**
+     * Test hasKey method
+     */
+
+    @Test
+    public void testHasKey_isInKey() {
+        Map<String, String> test = this.createFromArgsTest("James", "1", "Tony",
+                "2", "Bob", "3");
+        Map<String, String> expected = this.createFromArgsTest("James", "1",
+                "Tony", "2", "Bob", "3");
+        boolean testBoo = test.hasKey("James");
+        boolean expectedBoo = expected.hasKey("James");
+
+        assertEquals(expectedBoo, testBoo);
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testHasKey_notInKey() {
+        Map<String, String> test = this.createFromArgsTest("James", "1", "Tony",
+                "2", "Bob", "3");
+        Map<String, String> expected = this.createFromArgsTest("James", "1",
+                "Tony", "2", "Bob", "3");
+        boolean testBoo = test.hasKey("BooB");
+        boolean expectedBoo = expected.hasKey("BooB");
+
+        assertEquals(expectedBoo, testBoo);
+        assertEquals(expected, test);
+    }
+
+    /**
+     * Test size method
+     */
+    @Test
+    public void testIsSize1() {
+
+        Map<String, String> map = this.createFromArgsTest();
+        int test = map.size();
+        int expected = 0;
+
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testIsSize2() {
+
+        Map<String, String> map = this.createFromArgsTest("0", "0");
+        int test = map.size();
+        int expected = 1;
+
+        assertEquals(expected, test);
+    }
+
+    @Test
+    public void testIsSize3() {
+
+        Map<String, String> map = this.createFromArgsTest("0", "0", "1", "1",
+                "2", "2", "3", "3", "4", "4");
+        int test = map.size();
+        int expected = 5;
+
+        assertEquals(expected, test);
+    }
 }
