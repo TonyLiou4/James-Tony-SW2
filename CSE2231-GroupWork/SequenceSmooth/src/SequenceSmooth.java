55bf0e8cb313e613718d1c3e801bd06d0c8e4310
TonyLiou4 <tonyliou4@gmail.com>
2022-02-15 16:19:23 -0500

    sequenceOnStack

--- CSE2231-GroupWork/SequenceSmooth/src/SequenceSmooth.java
+++ CSE2231-GroupWork/SequenceSmooth/src/SequenceSmooth.java
@@ -55,41 +55,41 @@ public final class SequenceSmooth {
         s2.transferFrom(seq);
     }
 
-    /**
-     * Smooths a given {@code Sequence<Integer>}.
-     *
-     * @param s1
-     *            the sequence to smooth
-     * @returns s2 the smoothed sequence that is returned
-     * @requires |s1| >= 1
-     * @ensures <pre>
-     * |s2| = |s1| - 1  and
-     *  for all i, j: integer, a, b: string of integer
-     *      where (s1 = a * <i> * <j> * b)
-     *    (there exists c, d: string of integer
-     *       (|c| = |a|  and
-     *        s2 = c * <(i+j)/2> * d))
-     * </pre>
-     */
-   public static Sequence<Integer> smooth(Sequence<Integer> s1) {
-        assert s1 != null : "Violation of: s1 is not null";
-        assert s1.length() >= 1 : "Violation of: |s1| >= 1";
-
-       Sequence<Integer> s2 = new Sequence1L<>();
-        int pos = 0;
-
-        if (s1.length() == 1) {
-            return s2;
-        } else {
-            double first = s1.remove(0);
-            double second = s1.entry(1);
-            double avg = (first + second) / 2.0;
-
-            s2.add(pos, (int) avg);
-            pos++;
-
-            smooth(s1);
-        }
-    }
+//    /**
+//     * Smooths a given {@code Sequence<Integer>}.
+//     *
+//     * @param s1
+//     *            the sequence to smooth
+//     * @returns s2 the smoothed sequence that is returned
+//     * @requires |s1| >= 1
+//     * @ensures <pre>
+//     * |s2| = |s1| - 1  and
+//     *  for all i, j: integer, a, b: string of integer
+//     *      where (s1 = a * <i> * <j> * b)
+//     *    (there exists c, d: string of integer
+//     *       (|c| = |a|  and
+//     *        s2 = c * <(i+j)/2> * d))
+//     * </pre>
+//     */
+//    public static Sequence<Integer> smooth(Sequence<Integer> s1) {
+//        assert s1 != null : "Violation of: s1 is not null";
+//        assert s1.length() >= 1 : "Violation of: |s1| >= 1";
+//
+//        Sequence<Integer> s2 = new Sequence1L<>();
+//        int pos = 0;
+//
+//        if (s1.length() == 1) {
+//            return s2;
+//        } else {
+//            double first = s1.remove(0);
+//            double second = s1.entry(1);
+//            double avg = (first + second) / 2.0;
+//
+//            s2.add(pos, (int) avg);
+//            pos++;
+//
+//            smooth(s1);
+//        }
+//    }
 
 }
