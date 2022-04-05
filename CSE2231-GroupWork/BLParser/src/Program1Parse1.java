6aefcc5da4bcc73a598cbe7c161048da8e0f51b8
TonyLiou4 <tonyliou4@gmail.com>
2022-04-05 15:46:33 -0400

    fghjkolokiujhgfhjkolokjhgfcghjkl;

--- CSE2231-GroupWork/BLParser/src/Program1Parse1.java
+++ CSE2231-GroupWork/BLParser/src/Program1Parse1.java
@@ -6,12 +6,13 @@ import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
 import components.simplewriter.SimpleWriter1L;
 import components.statement.Statement;
+import components.utilities.Reporter;
 import components.utilities.Tokenizer;
 
 /**
  * Layered implementation of secondary method {@code parse} for {@code Program}.
  *
- * @author Put your name here
+ * @author Sungwoon Park and Tony Liou
  *
  */
 public final class Program1Parse1 extends Program1 {
@@ -56,10 +57,12 @@ public final class Program1Parse1 extends Program1 {
         assert tokens.length() > 0 && tokens.front().equals("INSTRUCTION") : ""
                 + "Violation of: <\"INSTRUCTION\"> is proper prefix of tokens";
 
-        // TODO - fill in body
+        String result = tokens.dequeue();
+        boolean something = false;
 
-        // This line added just to make the program compilable.
-        return null;
+        Reporter.assertElseFatalError(something, "name is not valid");
+
+        return result;
     }
 
     /*
@@ -91,7 +94,23 @@ public final class Program1Parse1 extends Program1 {
         assert tokens.length() > 0 : ""
                 + "Violation of: Tokenizer.END_OF_INPUT is a suffix of tokens";
 
-        // TODO - fill in body
+        //check if ord program exist
+        Reporter.assertElseFatalError( tokens.dequeue().equals("PROGRAM"), "Missing PROGRAM");
+        //save the name of file
+        String name = tokens.dequeue();
+        //name is not an identifier
+        Reporter.assertElseFatalError(Tokenizer.isIdentifier(name), "Name is not valid. ");
+        Reporter.assertElseFatalError( tokens.dequeue().equals("IS"), "Missing IS. ");
+        this.setName(name);
+
+        //loop for instructions blocks and call parse iNsructions
+        for(
+        Reporter.assertElseFatalError( tokens.dequeue().equals("IS"), "Missing IS. ");
+
+
+        temp.parseBlock(tokens);
+
+        //call parse INstruction or parse block
 
     }
 
